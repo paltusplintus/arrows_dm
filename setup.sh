a19f260be9f90b2418763bacd56de205c597944c
Ubuntu <ubuntu@ip-172-31-85-76.ec2.internal>
2021-12-20 17:50:06 +0000

    added upgrade pip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # sudo apt install python3-venv
 python3 -m venv venv
+venv/bin/python -m pip install --upgrade pip
 venv/bin/python -m pip install -r requirements.txt
 venv/bin/python -m jupyter nbextension enable --py --sys-prefix qgrid
 venv/bin/python -m jupyter nbextension enable --py --sys-prefix ipyvuetify
